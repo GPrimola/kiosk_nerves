ef51f890590d8a8682159a396ba2583b290739d0
Giorgio Torres <giorgio.torres@sumup.com>
2020-03-03 17:49:41 -0300

    Working Kiosk.

---
+++ lib/kiosk_nerves.ex
@@ -0,0 +1,20 @@
+defmodule KioskNerves do
+  require Logger
+  @moduledoc """
+  Documentation for KioskNerves.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KioskNerves.hello
+      :world
+
+  """
+  def hello do
+    Logger.debug("Hello World!")
+    :world
+  end
+end
